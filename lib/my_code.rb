dec4726c96c3bd3f91d9ab9cad50c06183f295a5
Victoria Cushing <victoriaccushing@gmail.com>
2019-07-17 05:50:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -47,8 +47,7 @@ end
 def reduce_to_all_true(source_array)
   i = 0 
   while i < source_array.length do
-    if source_array[i] != false 
-      return true
+    if source_array[i] != true 
     end
     i += 1 
   end
